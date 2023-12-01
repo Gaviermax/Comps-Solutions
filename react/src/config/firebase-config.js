1bc4cfddeaead6d20f5685ccc192fd69bf58b9b3
shifuuuuu <95745179+shifuuuuu@users.noreply.github.com>
2023-12-01 10:36:18 +0800

    created config file for database

---
+++ react/src/config/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getDatabase } from "firebase/database"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1nO8GEMuUryXWwLIi92eEPf5QfNYNhJ8",
+  authDomain: "loginsignup-801b6.firebaseapp.com",
+  databaseURL: "https://loginsignup-801b6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "loginsignup-801b6",
+  storageBucket: "loginsignup-801b6.appspot.com",
+  messagingSenderId: "409222316795",
+  appId: "1:409222316795:web:80c979400122d9e028665e",
+  measurementId: "G-TFTY28FEK6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);  
+export const database = getDatabase(app);
+
