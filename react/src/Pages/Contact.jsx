fe931d5906854c5424d91a09aee8df7d50b26611
shifuuuuu <95745179+shifuuuuu@users.noreply.github.com>
2023-12-02 03:12:04 +0800

    contact japon iya ngalan, kay diko kabalo saog change nga mulink ra japon, but naa diri ang admin booking info

--- react/src/Pages/Contact.jsx
+++ react/src/Pages/Contact.jsx
@@ -1,9 +1,37 @@
-function Contact(){
-    return(
-        <>
-            <h1>Contact</h1>
-        </>
-    )
-}
+// Contact.jsx
+import React, { useEffect, useState } from "react";
+import { db } from "../config/firebase-config";
+import BookingDisplay from "./BookingDisplay";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const Contact = () => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    // Fetch booking data from Firestore
+    const fetchBookings = async () => {
+      const bookingsCollection = collection(db, "bookings");
+      const bookingsSnapshot = await getDocs(bookingsCollection);
+
+      const bookingData = bookingsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+        
+      }));
+      console.log("Fetched Bookings:", bookingData);
+
+      setBookings(bookingData);
+    };
+
+    fetchBookings();
+  }, []); // Run once on component mount
+
+  return (
+    <>
+      <h1>Contact</h1>
+      <BookingDisplay bookings={bookings} />
+    </>
+  );
+};
 
 export default Contact;
