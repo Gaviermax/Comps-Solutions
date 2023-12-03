57589be29c48ad2b9e25833d2757e62873214b64
shifuuuuu <95745179+shifuuuuu@users.noreply.github.com>
2023-12-03 11:17:49 +0800

    added sign out function and appropriate navbar transitions

--- react/src/Pages/Layout.jsx
+++ react/src/Pages/Layout.jsx
@@ -6,19 +6,26 @@ import { auth } from "../config/firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
 import { Button } from "bootstrap";
 
-function Layout(){
+function Layout() {
     const [isAdmin, setIsAdmin] = useState(false);
-
-  useEffect(() => {
-    // Listen for changes in the authentication state
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      // Check if the user is logged in and has admin privileges
-      setIsAdmin(user && user.email === "admin@gmail.com");
-    });
-
-    // Clean up the subscription when the component unmounts
-    return () => unsubscribe();
-  }, []);
+    const [user, setUser] = useState(null);
+  
+    useEffect(() => {
+      // Listen for changes in the authentication state
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        // Check if the user is logged in and has admin privileges
+        setIsAdmin(user && user.email === "admin@gmail.com");
+        setUser(user);
+      });
+  
+      // Clean up the subscription when the component unmounts
+      return () => unsubscribe();
+    }, []);
+  
+    const handleSignOut = () => {
+      // Sign out the user
+      auth.signOut();
+    };
 
     return(
         <>
@@ -65,8 +72,30 @@ function Layout(){
 
                         </li>
                     </ul>
-                    <Link to="/signup"><button className="btn btn-outline-light fw-medium me-4 rounded-0" type="button">Sign Up</button></Link>
-                    <Link to="/login"><button className="btn btn-light text-black me-5 fw-medium rounded-0" type="button">Log In</button></Link>
+                    {user ? (
+                    <>
+                        <button
+                        onClick={handleSignOut}
+                        className="btn btn-outline-light fw-medium me-4 rounded-0"
+                        type="button"
+                        >
+                        Sign Out
+                        </button>
+                    </>
+                    ) : (
+                    <>
+                        <Link to="/signup">
+                        <button className="btn btn-outline-light fw-medium me-4 rounded-0" type="button">
+                            Sign Up
+                        </button>
+                        </Link>
+                        <Link to="/login">
+                        <button className="btn btn-light text-black me-5 fw-medium rounded-0" type="button">
+                            Log In
+                        </button>
+                        </Link>
+                    </>
+                    )}
                 </form>
                 </div>
             </div>
