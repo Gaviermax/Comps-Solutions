ace22ba5174bb0be73edb7e24ba614b1c4f8da01
shifuuuuu <95745179+shifuuuuu@users.noreply.github.com>
2023-12-03 11:03:07 +0800

    added "Delete" function for the admin

--- react/src/Pages/BookingDisplay.jsx
+++ react/src/Pages/BookingDisplay.jsx
@@ -1,10 +1,35 @@
-// BookingDisplay.jsx
 import React from "react";
+import { deleteDoc, doc, getDoc } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
-const BookingDisplay = ({ bookings }) => {
+const BookingDisplay = ({ bookings, onBookingDelete }) => {
+  const db = getFirestore();
+
+  const handleDelete = async (bookingId) => {
+    try {
+      // Delete the booking from Firestore
+      await deleteDoc(doc(db, 'bookings', bookingId));
+
+      // Check if the document still exists after deletion
+      const bookingDoc = await getDoc(doc(db, 'bookings', bookingId));
+
+      if (!bookingDoc.exists()) {
+        // Show success alert after successful deletion
+        alert('Booking has been successfully deleted! Refresh the page to see changes.');
+      } else {
+        // Show error alert if deletion fails
+        alert('Error deleting booking. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error deleting document: ', error);
+      // Show error alert if deletion fails
+      alert('Error deleting booking. Please try again.');
+    }
+  };
+
   return (
     <div className="container shadow rounded my-3 p-3">
-      <h1 className="text-center pt-4 pb-2">Admin List of Reservation</h1>
+      <h1 className="text-center pt-4 pb-2">Admin's List of Bookings</h1>
       <table className="table table-striped" id="datatable">
         <thead>
           <tr>
@@ -15,6 +40,7 @@ const BookingDisplay = ({ bookings }) => {
             <th>Email</th>
             <th>Type</th>
             <th>Date</th>
+            <th>Action</th> {/* New column for the delete button */}
           </tr>
         </thead>
         <tbody id="tableArea">
@@ -27,6 +53,13 @@ const BookingDisplay = ({ bookings }) => {
               <td>{booking.email}</td>
               <td>{booking.bookingType}</td>
               <td>{booking.date}</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(booking.id)}
+                  className="btn btn-danger">
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
